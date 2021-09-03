7cf2466afa11836be12bc6917c9365226d884ac9
Felix Verdugo <felixalbertocsl@gmail.com>
2021-09-03 14:21:59 -0600

    Estilos Listos

--- app.js
+++ app.js
@@ -1,4 +1,41 @@
-const button = document.getElementById("button")
-button.addEventListener("click", ()=>{
-  alert("Hola")
-})
+const inputFigura = document.getElementById("inputFigura");
+const inputLado = document.getElementById("inputLado");
+const buttonA = document.getElementById("buttonA");
+const buttonP = document.getElementById("buttonP");
+
+buttonA.addEventListener("click", (e) => {
+  //   if (inputFigura.value == "cuadrado" || inputFigura.value == "Cuadrado") {
+  //     areaCuadrado(inputLado.value);
+  //   }else if (inputFigura.value == "circulo"){
+  //     areaCirculo(inputLado.value)
+  //   }else if(){
+  //   }
+});
+
+// const areaCuadrado = (ladoMedida) => {
+//   let result = ladoMedida * ladoMedida;
+//   return console.log(result)
+// };
+// const areaCirculo = (r) => {
+//   let result = Math.PI * (Math.pow(r, 2));
+//   return console.log(result)
+// };
+
+// const figuras = [
+//   {
+//     figura: "circulo",
+//     operacion: Math.PI * Math.pow(r, 2),
+//   },
+//   {
+//     figura: "cuadrado",
+//     operacion: b * b,
+//   },
+//   {
+//     figura: "rombo",
+//     operacion: (D * d * a) / 3,
+//   },
+//   {
+//     figura: "rectangulo",
+//     operacion: b * a,
+//   },
+// ];
