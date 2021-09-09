99def97c20f7c0d648f35af32d8a4737a2acd1d1
Felix <felixalbertocsl@gmail.com>
2021-09-08 20:33:22 -0600

    Cambios en app.js

--- app.js
+++ app.js
@@ -3,27 +3,16 @@ const inputLado = document.getElementById("inputLado");
 const buttonA = document.getElementById("buttonA");
 const buttonP = document.getElementById("buttonP");
 
-// const figuras = [
-//   {
-//     figura: "circulo",
-//     operacion: Math.PI * Math.pow(r, 2),
-//   },
-//   {
-//     figura: "cuadrado",
-//     operacion: b * b,
-//   },
-//   {
-//     figura: "rombo",
-//     operacion: (D * d * a) / 3,
-//   },
-//   {
-//     figura: "rectangulo",
-//     operacion: b * a,
-//   },
-// ];
-
-buttonA.addEventListener("click", (e) => {
-  
+buttonA.addEventListener("click", () => {
+  for (const i of figuras) {
+    if (i.figura == selectFigure.value && inputLado.value != 0) {
+      console.log(i.operacion(inputLado.value) + " " + selectFigure.value);
+      break;
+    } else {
+      alert("Nos has seleccionado figura O Numero");
+      break;
+    }
+  }
 });
 
 // const areaCuadrado = (ladoMedida) => {
@@ -34,4 +23,3 @@ buttonA.addEventListener("click", (e) => {
 //   let result = Math.PI * (Math.pow(r, 2));
 //   return console.log(result)
 // };
-
