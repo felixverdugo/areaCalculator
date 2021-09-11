c4a5e9f982f14064ccd3b529bcfd2de56dfe75ca
Felix <felixalbertocsl@gmail.com>
2021-09-10 22:25:30 -0600

    Mensajes cambiados en los console.log

--- app.js
+++ app.js
@@ -4,24 +4,21 @@ const buttonA = document.getElementById("buttonA");
 const buttonP = document.getElementById("buttonP");
 
 //  Formula Cuadrado
-console.group(
+console.groupCollapsed(
   "%cCuadrado",
   "color:#b8fffe; font-family:Arial; padding:10px; font-size:14px;"
 );
-const cuadrado_l1 = 1;
-const cuadrado_l2 = 1;
+const cuadrado_l = 1;
 console.log(
-  `%cEl resultado de  ${cuadrado_l1} x ${cuadrado_l1} = ${
-    cuadrado_l1 + cuadrado_l2
-  }`,
+  `%cEl area del cuadrado es de ${cuadrado_l * cuadrado_l}`,
   "color: hotpink; padding:7px; font-family:Arial; font-size:12px;"
 );
 console.log(
-  `%cLado 1 de tu cuadrado es de ${cuadrado_l1}`,
-  "color: purple; font-family: Arial; padding:5px;"
+  `%cEl Perimetro de tu cuadrado  es de ${cuadrado_l * 4}`,
+  "color: hotpink; padding:7px; font-family:Arial; font-size:12px;"
 );
 console.log(
-  `%cLado 2 de tu cuadrado es de ${cuadrado_l2}`,
+  `%cLado de tu cuadrado es de ${cuadrado_l}`,
   "color: purple; font-family: Arial; padding:5px;"
 );
 console.groupEnd();
@@ -31,46 +28,43 @@ console.groupEnd();
 const triangulo_b = 20;
 const triangulo_a = 10;
 
-console.group(
+console.groupCollapsed(
   "%cTriangulo",
   "color:#ffe06e; font-family:Arial; padding:10px; font-size:14px;"
 );
 console.log(
-  `%cEl resultado de  ${triangulo_a} x ${triangulo_b} / 2 = ${
-    (triangulo_a * triangulo_b) / 2
-  }`,
+  `%cEl área de tu triangulo es de ${(triangulo_a * triangulo_b) / 2}`,
   "color: hotpink; font-family:Arial; padding:7px; font-size:12px;"
 );
 console.log(
-  `%cLado 1 de tu cuadrado es de ${cuadrado_l1}`,
+  `%cLa base de tu triangulo es de ${triangulo_b}`,
   "font-family: Arial; color: pink; padding:5px;"
 );
 console.log(
-  `%cLado 2 de tu cuadrado es de ${cuadrado_l2}`,
+  `%cLa altura de tu triangulo es de ${triangulo_a}`,
   "font-family: Arial; color: pink; padding:5px;"
 );
+
 console.groupEnd();
 
 // Formula Rectangulo
 const rectangulo_b = 10;
 const rectangulo_a = 5;
 
-console.group(
+console.groupCollapsed(
   "%cRectangulo",
   "color:#3b65ff; font-family:Arial; padding:10px; font-size:14px;"
 );
 console.log(
-  `%cEl resultado de  ${rectangulo_a} x ${rectangulo_b} = ${
-    rectangulo_b * rectangulo_a
-  }`,
+  `%cEl área de tu rectangulo es de ${rectangulo_b * rectangulo_a}`,
   "color: hotpink; padding:7px; font-family:Arial; font-size:12px;"
 );
 console.log(
-  `%cLado 1 de tu cuadrado es de ${rectangulo_a}`,
+  `%cLa base de tu rectangulo es de ${rectangulo_b}`,
   "font-family: Arial; color: orange; padding:5px;"
 );
 console.log(
-  `%cLado 2 de tu cuadrado es de ${rectangulo_b}`,
+  `%cLa altura de tu rectangulo es de ${rectangulo_a}`,
   "font-family: Arial; color: orange; padding:5px;"
 );
 console.groupEnd();
